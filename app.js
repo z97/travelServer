1dc36eb8d9c7451581b55410f34612947fe75e6b
kliba <klibaner@mail.com>
2022-05-05 09:53:21 +0300

    1

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res){
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
+  app.listen(4000);
+  console.log('Express started on port 4000');
 }
